41c7c1f5779c235c8c739eb0dc23107c534a7418
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-08-31 02:18:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,15 @@ function getNumbers() {
 }
 
 function filterEvenNumbers(numbers) {
-    return new Promise((resolve) => {
-        const evenNumbers = numbers.filter(num => num % 2 === 0);
-        document.getElementById("output").innerText = evenNumbers.join(', ');
-        resolve(evenNumbers);
-    });
+    const evenNumbers = numbers.filter(num => num % 2 === 0);
+    document.getElementById("output").innerText = evenNumbers.join(', ');
+    return Promise.resolve(evenNumbers);
 }
 
 function multiplyNumbers(numbers) {
-    return new Promise((resolve) => {
-        const multipliedNumbers = numbers.map(num => num * 2);
-        document.getElementById("output").innerText = multipliedNumbers.join(', ');
-        resolve(multipliedNumbers);
-    });
+    const multipliedNumbers = numbers.map(num => num * 2);
+    document.getElementById("output").innerText = multipliedNumbers.join(', ');
+    return Promise.resolve(multipliedNumbers);
 }
 
 getNumbers()
